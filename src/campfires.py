b8464fd38cc8c5619d15c163b824321285668229
dolliou <antoine.dolliou@gmail.com>
2026-09-24 20:15:01 +0200

    correct

--- src/campfires.py
+++ src/campfires.py
@@ -307,7 +307,7 @@ class Stack:
             shmm_slices, slices = gen_shmm(
                 create=False, **self._slices_dict
             )
-            s                   = list(slices[i])
+            s                   = tuple(slices[i])
             blob_data           = blobs_data[s]
             region              = regions[s]        
 
